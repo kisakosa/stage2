c242993bc563e4644f3f9f62158e08ad248d7788
kisakosa <113772579+kisakosa@users.noreply.github.com>
2024-10-16 15:28:29 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 8080;
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact-me.html'));
+});
+
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
